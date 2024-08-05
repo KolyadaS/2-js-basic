3fa93182842266eb4df82ee71b4422c8a641b34f
Svetlana Kolyada <pfotka@yandex.ru>
2024-08-05 15:42:21 +0400

    5-languages

---
+++ 5-languages/app.js
@@ -0,0 +1,15 @@
+const lang = "ru";
+
+switch (lang) {
+  case "ru":
+    console.log("Здравствуйте!");
+    break;
+  case "en":
+    console.log("Hi!");
+    break;
+  case "de":
+    console.log("Gutten tag!");
+    break;
+  default:
+    console.log("Hello!");
+}
