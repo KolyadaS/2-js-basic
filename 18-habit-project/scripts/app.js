c65496994bf6b8812d317be4187ff98859eb912e
Svetlana Kolyada <pfotka@yandex.ru>
2024-09-21 21:28:25 +0400

    Оживление меню и контента

--- 18-habit-project/scripts/app.js
+++ 18-habit-project/scripts/app.js
@@ -1,3 +1,145 @@
 "use strict";
 
-console.log("qwerty");
+let habits = [];
+const HABIT_KEY = "HABIT_KEY";
+let globalActiveHabitId;
+
+// page
+const page = {
+  menu: document.querySelector(".menu__list"),
+  header: {
+    h1: document.querySelector(".h1"),
+    progressPercent: document.querySelector(".progress__percent"),
+    progressCoverBar: document.querySelector(".progress__cover-bar"),
+  },
+  content: {
+    daysContainer: document.getElementById("days"),
+    nextDay: document.querySelector(".habit__day"),
+  },
+};
+
+// utils
+
+function loadData() {
+  const habitsString = localStorage.getItem(HABIT_KEY);
+  const habitArray = JSON.parse(habitsString);
+  if (Array.isArray(habitArray)) {
+    habits = habitArray;
+  }
+}
+
+function saveData() {
+  localStorage.setItem(HABIT_KEY, JSON.stringify(habits));
+}
+
+// render
+function rerenderMenu(activeHabit) {
+  for (const habit of habits) {
+    const existed = document.querySelector(`[menu-habit-id="${habit.id}"]`);
+    if (!existed) {
+      const element = document.createElement("button");
+      element.setAttribute("menu-habit-id", habit.id);
+      element.classList.add("menu__item");
+      element.addEventListener("click", () => rerender(habit.id));
+      element.innerHTML = `<img src="./images/${habit.icon}.svg" alt="${habit.icon}" />`;
+      if (activeHabit.id === habit.id) {
+        element.classList.add("menu__item_active");
+      }
+      page.menu.appendChild(element);
+      continue;
+    }
+    if (activeHabit.id === habit.id) {
+      existed.classList.add("menu__item_active");
+    } else {
+      existed.classList.remove("menu__item_active");
+    }
+  }
+}
+
+function rerenderHead(activeHabit) {
+  page.header.h1.innerText = activeHabit.name;
+  const progress =
+    activeHabit.days.length / activeHabit.target > 1
+      ? 100
+      : (activeHabit.days.length / activeHabit.target) * 100;
+  page.header.progressPercent.innerText = progress.toFixed(0) + "%";
+  page.header.progressCoverBar.setAttribute("style", `width: ${progress}%`);
+}
+
+function rerenderContent(activeHabit) {
+  page.content.daysContainer.innerHTML = "";
+  for (const index in activeHabit.days) {
+    const element = document.createElement("div");
+    element.classList.add("habit");
+    element.innerHTML = `<div class="habit__day">День ${Number(index) + 1}</div>
+              <div class="habit__comment">${
+                activeHabit.days[index].comment
+              }</div>
+              <button class="habit_delete" onclick="deleteDay(${index})">
+                <img src="./images/delete.svg" alt="Удалить день ${Number(
+                  index + 1
+                )}" />
+              </button>`;
+    page.content.daysContainer.appendChild(element);
+  }
+  page.content.nextDay.innerHTML = `День ${activeHabit.days.length + 1}`;
+}
+
+function rerender(activeHabitId) {
+  globalActiveHabitId = activeHabitId;
+  const activeHabit = habits.find((habit) => habit.id === activeHabitId);
+  if (!activeHabit) {
+    return;
+  }
+  rerenderMenu(activeHabit);
+  rerenderHead(activeHabit);
+  rerenderContent(activeHabit);
+}
+
+// work with days
+function addDays(event) {
+  const form = event.target;
+  event.preventDefault();
+  const data = new FormData(form);
+  const comment = data.get("comment");
+  form["comment"].classList.remove("error");
+  if (!comment) {
+    form["comment"].classList.add("error");
+    return;
+  }
+  habits = habits.map((habit) => {
+    console.log(habits);
+    console.log(globalActiveHabitId);
+    if (habit.id === globalActiveHabitId) {
+      return {
+        ...habit,
+        days: habit.days.concat([{ comment }]),
+      };
+    }
+    return habit;
+  });
+  form["comment"].value = "";
+  rerender(globalActiveHabitId);
+  saveData();
+}
+
+function deleteDay(index) {
+  habits = habits.map((habit) => {
+    if (habit.id === globalActiveHabitId) {
+      habit.days.splice(index, 1);
+      return {
+        ...habit,
+        days: habit.days,
+      };
+    }
+    return habit;
+  });
+  rerender(globalActiveHabitId);
+  saveData();
+}
+
+// init
+(() => {
+  loadData();
+  rerender(habits[0].id);
+})();
